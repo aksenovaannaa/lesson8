90d0f884b0655875218c90f3251d3536267411be
aksenovaannaa <anya.axenova2011@gmail.com>
2024-11-28 21:50:45 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,44 @@
+#вариант 1
+first=147
+second=147
+third=147
+if first==second and first==third:
+    print(3)
+elif first==second:
+    print(2)
+elif first==third:
+    print(2)
+elif second==third:
+    print(2)
+elif first!=second and first!=third and second!=third:
+    print(0)
+
+#вариант 2
+first=148
+second=147
+third=147
+if first==second and first==third:
+    print(3)
+elif first==second:
+    print(2)
+elif first==third:
+    print(2)
+elif second==third:
+    print(2)
+elif first!=second and first!=third and second!=third:
+    print(0)
+
+#вариант 3
+first=147
+second=148
+third=149
+if first==second and first==third:
+    print(3)
+elif first==second:
+    print(2)
+elif first==third:
+    print(2)
+elif second==third:
+    print(2)
+elif first!=second and first!=third and second!=third:
+    print(0)
